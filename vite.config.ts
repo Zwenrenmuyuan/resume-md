33cff9e4299e646173337da164de6b8b8f27516c
wenrenmuyuan <3106133216@qq.com>
2026-05-18 11:18:35 +0800

    ci: 添加 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/resume-md/',
   server: { port: 7788, strictPort: true, open: true },
 });
